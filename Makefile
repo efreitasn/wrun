7a088dfc42293dbf575b2dd83f810bc4e1d73988
Emanuel Freitas <efreitasn@hotmail.com>
2020-01-02 02:51:14 -0300

    Add Makefile

---
+++ Makefile
@@ -0,0 +1,10 @@
+VERSION=$(shell git describe --abbrev=0 --tags)
+NAME="wrun"
+
+build: export GOOS?=$(shell go env GOOS)
+build: export GOARCH?=$(shell go env GOARCH)
+build:
+	@echo "Building wrun@${VERSION} for ${GOOS}/${GOARCH}"
+	@go build -ldflags="-X github.com/efreitasn/wrun/cmd/wrun/internal/cmds.version=${VERSION}" \
+		-o ${NAME} github.com/efreitasn/wrun/cmd/wrun
+	@echo "Build completed"
--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LATEST_TAG=$(git tag | tail -1)
+VERSION=$(git describe --abbrev=0 --tags)
 TARGETS=("linux/amd64" "darwin/amd64")
 
 for TARGET in "${TARGETS[@]}"; do
@@ -8,7 +8,7 @@ for TARGET in "${TARGETS[@]}"; do
   OS=${TARGET_PARTS[0]}
   ARCH=${TARGET_PARTS[1]}
 
-  GOOS=$OS GOARCH=$ARCH go build -o "build/wrun-${LATEST_TAG}-${OS}-${ARCH}" ./cmd/wrun
+  make -B build NAME="build/wrun-${VERSION}-${OS}-${ARCH}" GOOS=$OS GOARCH=$ARCH
 
   if [ $? -ne 0 ]; then
     echo "Error while creating binary for ${OS}-${ARCH}."
