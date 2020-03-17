ebad838178cfc0c09228077889fd357f2c13e9b0
Emanuel Freitas <efreitasn@hotmail.com>
2020-03-17 19:34:30 -0300

    build/install/release: fix/update instructions
    Close #7.

--- release.sh
+++ release.sh
@@ -25,17 +25,5 @@ for TARGET in "${TARGETS[@]}"; do
 
   echo "${OS}-${ARCH}"
 
-  make -B build NAME="release/wrun" GOOS=$OS GOARCH=$ARCH
-
-  echo "creating tarball"
-  tar -czf "release/wrun-${VERSION}-${OS}-${ARCH}.tar.gz" --transform="s/release\///" \
-    release/wrun completion.sh Makefile INSTALL
-  echo "tarball created"
-
-  echo "creating zip"
-  zip -qj "release/wrun-${VERSION}-${OS}-${ARCH}.zip" \
-    release/wrun completion.sh Makefile INSTALL
-  echo "zip created"
-
-  rm release/wrun
+  make -B build NAME="release/wrun-${VERSION}-${OS}-${ARCH}" GOOS=$OS GOARCH=$ARCH
 done
