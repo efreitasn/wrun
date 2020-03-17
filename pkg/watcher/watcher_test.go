c10ad7b374d809d11da6ba1e8c32438b81c6b07a
Emanuel Freitas <efreitasn@hotmail.com>
2020-03-17 01:33:09 -0300

    pkg/watcher: fix typo in ModifyEvent

--- pkg/watcher/watcher_test.go
+++ pkg/watcher/watcher_test.go
@@ -478,8 +478,7 @@ func TestWatcher_modifyEvent(t *testing.T) {
 		}
 
 		expectedEvent := ModifyEvent{
-			isDir: false,
-			path:  path.Join(workingDir, filePath),
+			path: path.Join(workingDir, filePath),
 		}
 
 		select {
